b68fd251e03207a54453f1a111e68d648b5b5c90
Andrew K <drummer4life93@yahoo.com>
2020-05-14 15:43:13 -0500

    init

---
+++ LemonadeStand/Weather.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class Weather
+    {
+        public string condition;
+        public int temperature;
+        List<string> weatherConditions;
+        public string predictedForecast;
+    }
+}
